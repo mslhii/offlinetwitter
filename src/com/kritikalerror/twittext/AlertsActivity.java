696792bf0841cbbb57bfa3a64c071905c425ffd2
Michael <mslhii@yahoo.com>
2014-11-06 23:51:46 -0800

    Began AlertsActivity file

---
+++ src/com/kritikalerror/twittext/AlertsActivity.java
@@ -0,0 +1,17 @@
+package com.kritikalerror.twittext;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.kritikalerror.twittext.R;
+
+public class AlertsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+    }
+
+}
--- src/com/kritikalerror/twittext/LoginActivity.java
+++ src/com/kritikalerror/twittext/LoginActivity.java
@@ -17,7 +17,6 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import android.app.Activity;
-import android.content.ContentValues;
 import android.content.Intent;
 
 public class LoginActivity extends Activity {
